d235ababb7ce34c2bdc116bec1aaf648f2e88d85
Nadeem Khedr <nadeemkhedr@gmail.com>
2016-02-07 16:21:25 +0200

    Rename App.js to Hello-World.js

--- 1-setting-up-the-environment/src/js/app.js
+++ 1-setting-up-the-environment/src/js/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/App";
+import HelloWorld from "./components/Hello-World";
 
 const app = document.getElementById('app');
-ReactDOM.render(<App/>, app);
+ReactDOM.render(<HelloWorld/>, app);
